0389a05612e0e004c91cdc727a62ebac8d6a9f39
MattCanc <90893840+MattCanc@users.noreply.github.com>
2023-12-20 17:13:48 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from neo4j import GraphDatabase
-
-uri = 'bolt+s://dd77fa45.databases.neo4j.io'
-
-user = 'neo4j'
-pwd = 'PWZJs7q3meMtgeUQobxKOcMYuvFs9P36n2FNhi25gDs'
-driver = GraphDatabase.driver(uri, auth=(user, pwd)) # uri, username, pwd
-session = driver.session()
