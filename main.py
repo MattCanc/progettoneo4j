6e8f89a66dddcf64318a62e226756b35739df1ad
Matteo Canciani <matteo.canciani.03@gmail.com>
2024-01-11 11:44:24 +0100

    piccola modifica, aggiunta try catch

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from neo4j import GraphDatabase
+try:
+    uri = 'bolt+s://dd77fa45.databases.neo4j.io'
 
-uri = 'bolt+s://dd77fa45.databases.neo4j.io'
-
-user = 'neo4j'
-pwd = 'PWZJs7q3meMtgeUQobxKOcMYuvFs9P36n2FNhi25gDs'
-driver = GraphDatabase.driver(uri, auth=(user, pwd)) # uri, username, pwd
-session = driver.session()
+    user = 'neo4j'
+    pwd = 'PWZJs7q3meMtgeUQobxKOcMYuvFs9P36n2FNhi25gDs'
+    driver = GraphDatabase.driver(uri, auth=(user, pwd)) # uri, username, pwd
+    session = driver.session()
+    print('successo')
+except:
+    print('errore')
